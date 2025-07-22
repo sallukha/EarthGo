a8c2e22d338e5302a6b6a2e77b8229b26c9546bf
sallu khan <sallukhan54159@gmail.com>
2025-07-22 21:24:35 +0530

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Routes from './Routes/Routes'
 const App = () => {
   return (
-    <div className='p-5'>
+    <div className='p-5 overflow-hidden'>
        <Routes/>
     </div>
   )
